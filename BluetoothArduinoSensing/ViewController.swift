2f587c78339a180032257f4f6a9160090c927f57
ovallner <ovallner@gmail.com>
2017-10-25 22:02:30 -0500

    init

---
+++ BluetoothArduinoSensing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothArduinoSensing
+//
+//  Created by Oscar on 10/25/17.
+//  Copyright © 2017 SMU.cse5323. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
